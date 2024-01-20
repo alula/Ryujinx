30e66158bc4bab68aa510ee615cfb44ef22df28b
Alula <6276139+alula@users.noreply.github.com>
2024-01-20 15:30:58 +0100

    Add stubs for audctl playreport shit

--- src/Ryujinx.HLE/HOS/Services/Audio/IAudioController.cs
+++ src/Ryujinx.HLE/HOS/Services/Audio/IAudioController.cs
@@ -79,5 +79,104 @@ namespace Ryujinx.HLE.HOS.Services.Audio
 
             return ResultCode.Success;
         }
+
+        [CommandCmif(10000)]
+        // NotifyAudioOutputTargetForPlayReport()
+        public ResultCode NotifyAudioOutputTargetForPlayReport(ServiceCtx context)
+        {
+            Logger.Stub?.PrintStub(LogClass.ServiceAudio);
+
+            return ResultCode.Success;
+        }
+
+        [CommandCmif(10001)]
+        // NotifyAudioOutputChannelCountForPlayReport()
+        public ResultCode NotifyAudioOutputChannelCountForPlayReport(ServiceCtx context)
+        {
+            Logger.Stub?.PrintStub(LogClass.ServiceAudio);
+
+            return ResultCode.Success;
+        }
+
+        [CommandCmif(10002)]
+        // NotifyUnsupportedUsbOutputDeviceAttachedForPlayReport()
+        public ResultCode NotifyUnsupportedUsbOutputDeviceAttachedForPlayReport(ServiceCtx context)
+        {
+            Logger.Stub?.PrintStub(LogClass.ServiceAudio);
+
+            return ResultCode.Success;
+        }
+
+        [CommandCmif(10100)]
+        // GetAudioVolumeDataForPlayReport()
+        public ResultCode GetAudioVolumeDataForPlayReport(ServiceCtx context)
+        {
+            Logger.Stub?.PrintStub(LogClass.ServiceAudio);
+
+            return ResultCode.Success;
+        }
+
+        [CommandCmif(10101)]
+        // BindAudioVolumeUpdateEventForPlayReport()
+        public ResultCode BindAudioVolumeUpdateEventForPlayReport(ServiceCtx context)
+        {
+            Logger.Stub?.PrintStub(LogClass.ServiceAudio);
+
+            return ResultCode.Success;
+        }
+
+        [CommandCmif(10102)]
+        // BindAudioOutputTargetUpdateEventForPlayReport()
+        public ResultCode BindAudioOutputTargetUpdateEventForPlayReport(ServiceCtx context)
+        {
+            Logger.Stub?.PrintStub(LogClass.ServiceAudio);
+
+            return ResultCode.Success;
+        }
+
+        [CommandCmif(10103)]
+        // GetAudioOutputTargetForPlayReport()
+        public ResultCode GetAudioOutputTargetForPlayReport(ServiceCtx context)
+        {
+            Logger.Stub?.PrintStub(LogClass.ServiceAudio);
+
+            return ResultCode.Success;
+        }
+
+        [CommandCmif(10104)]
+        // GetAudioOutputChannelCountForPlayReport()
+        public ResultCode GetAudioOutputChannelCountForPlayReport(ServiceCtx context)
+        {
+            Logger.Stub?.PrintStub(LogClass.ServiceAudio);
+
+            return ResultCode.Success;
+        }
+
+        [CommandCmif(10105)]
+        // BindAudioOutputChannelCountUpdateEventForPlayReport()
+        public ResultCode BindAudioOutputChannelCountUpdateEventForPlayReport(ServiceCtx context)
+        {
+            Logger.Stub?.PrintStub(LogClass.ServiceAudio);
+
+            return ResultCode.Success;
+        }
+
+        [CommandCmif(10106)]
+        // GetDefaultAudioOutputTargetForPlayReport()
+        public ResultCode GetDefaultAudioOutputTargetForPlayReport(ServiceCtx context)
+        {
+            Logger.Stub?.PrintStub(LogClass.ServiceAudio);
+
+            return ResultCode.Success;
+        }
+
+        [CommandCmif(50000)]
+        // SetAnalogInputBoostGainForPrototyping()
+        public ResultCode SetAnalogInputBoostGainForPrototyping(ServiceCtx context)
+        {
+            Logger.Stub?.PrintStub(LogClass.ServiceAudio);
+
+            return ResultCode.Success;
+        }
     }
 }
