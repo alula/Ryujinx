ff84a8d86659672e39c0b0b81816587f9057f198
Alula <6276139+alula@users.noreply.github.com>
2025-03-04 21:26:13 +0100

    don't boot overlayDisp yet

--- src/Ryujinx.HLE/Switch.cs
+++ src/Ryujinx.HLE/Switch.cs
@@ -186,7 +186,8 @@ namespace Ryujinx.HLE
             LoadSystemProgramIfSupportedOnCurrentFirmware(SystemProgramId.Npns.Value, "npns:s");
             // LoadSystemProgramIfSupportedOnCurrentFirmware(SystemProgramId.Fatal.Value, "fatal:u"); // requires bus reimpl
 
-            LoadSystemProgramId(0x010000000000100C);
+            // LoadSystemProgramId(0x010000000000100C); // overlayDisp
+            // LoadSystemProgramId(0x0100000000001000); // qlaunch
         }
 
         public bool LoadCart(string exeFsDir, string romFsFile = null)
